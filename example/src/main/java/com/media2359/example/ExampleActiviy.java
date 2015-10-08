7ba9590ddd9ddd9d255fdf07613c4ff508f27bab
Phu Tang <phu.tanghong@2359media.com.vn>
2015-10-08 15:04:52 +0700

    update asr mode

--- example/src/main/java/com/media2359/example/ExampleActiviy.java
+++ example/src/main/java/com/media2359/example/ExampleActiviy.java
@@ -3,22 +3,16 @@ package com.media2359.example;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
+import android.widget.CheckBox;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import com.media2359.example.database.DBJobAdapter;
 import com.media2359.workvoice.WorkVoice;
-import com.media2359.workvoice.listener.WorkVoiceListener;
+import com.media2359.workvoice.i2r.helper.Constant;
 import com.media2359.workvoice.listener.WorkVoiceRegisterListener;
-import com.media2359.workvoice.model.WorkvoiceModel;
-
-import java.util.List;
 
 import de.greenrobot.event.EventBus;
 
@@ -29,6 +23,7 @@ public class ExampleActiviy extends Activity {
 
     ListView lv;
     JobAdapter adapter;
+    CheckBox cbOfflineASR;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,32 +44,44 @@ public class ExampleActiviy extends Activity {
         //step 1: register with workvoice system.
         String gcmSenderId ="1223805314"; //ask your system admin your gcmSender id, we will use it for register your device with Google cloud message
         String userId =""; //your system uniqueID, it should be user id, It define by your system to query when need to send a command.
-        WorkVoice.getInstance().register(gcmSenderId, userId,getApplicationContext(), new WorkVoiceRegisterListener() {
+        WorkVoice.getInstance().register(gcmSenderId, userId, getApplicationContext(), new WorkVoiceRegisterListener() {
             @Override
             public void onInit() {
                 //start register. You can to something like show dialog popup
-                Log.d("gcm","init");
+                Log.d("gcm", "init");
             }
 
             @Override
             public void onProgressUpdate() {
                 //TODO we will update later
-                Log.d("gcm","update");
+                Log.d("gcm", "update");
             }
 
             @Override
             public void onComplete(int resultCode) {
                 // return the final result of register (register with gcm success or not, register with workvoice sytem or not.
-                Log.d("gcm","success="+resultCode);
+                Log.d("gcm", "success=" + resultCode);
             }
         });
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 JobModel jobModel = (JobModel) lv.getItemAtPosition(i);
-                Intent intent = new Intent(ExampleActiviy.this,DetailActivity.class);
-                intent.putExtra("data",jobModel.getId());
+                Intent intent = new Intent(ExampleActiviy.this, DetailActivity.class);
+                intent.putExtra("data", jobModel.getId());
                 startActivity(intent);
+            }
+        });
+        cbOfflineASR = (CheckBox) findViewById(R.id.cb);
+        cbOfflineASR.setChecked(WorkVoice.getInstance().getAsrMode() == Constant.ASR_MODE.OFFLINE_MODE);
+        cbOfflineASR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (cbOfflineASR.isChecked()) {
+                    WorkVoice.getInstance().setAsrMode(Constant.ASR_MODE.OFFLINE_MODE);
+                } else {
+                    WorkVoice.getInstance().setAsrMode(Constant.ASR_MODE.ONLINE_MODE);
+                }
             }
         });
     }
